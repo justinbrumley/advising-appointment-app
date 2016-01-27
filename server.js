3c5351c6074c717ded00490ac550344a2a15910e
gopops1225 <gopops1225@gmail.com>
2016-01-27 23:30:43 +0000

    Updated port variable for c.9

--- server.js
+++ server.js
@@ -3,10 +3,13 @@
 var express = require('express');
 var app = express();
 
-var port = 8080;
+var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
   res.send('Hello there');
 });
 
-app.listen(port);
+app.listen(port, function(){
+    console.log("listening on port 8080");
+
+});
