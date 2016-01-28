688416944a3a9544210b34c3a7520a3935d087ee
Justin Brumley <justinbrumley012@gmail.com>
2016-01-28 10:23:48 -0600

    Set up static file server with Express and a little bit of file structure.

--- server.js
+++ server.js
@@ -5,11 +5,8 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.get('/', function(req, res) {
-  res.send('Hello there');
-});
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port, function(){
-    console.log("listening on port 8080");
-
+    console.log("NodeJS Server running on port " + port);
 });
