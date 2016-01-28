d1c9da10bfab15e359dd56475963eb3e6535497a
Justin Brumley <justinbrumley012@gmail.com>
2016-01-28 10:46:56 -0600

    Set up Jade rendering engine.

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
+// Set express app settings
+app.set('view engine', 'jade');
+app.set ('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
 
 app.listen(port, function(){
     console.log("NodeJS Server running on port " + port);
