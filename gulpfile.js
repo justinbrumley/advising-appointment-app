6c327c17b41cb9c274938532ffdc385588753571
Justin <justinbrumley012@gmail.com>
2016-02-02 01:31:16 +0000

    Code cleanup, added some images from ulm.edu, added section for mobile styles.

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-   gulp.src(['./assets/scss/*.scss', './assets/scss/**/*.scss'])
+  gulp.src(['./assets/scss/*.scss', './assets/scss/**/*.scss'])
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public/css'));
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('sass:watch', function() {
   gulp.watch(['./assets/scss/**/*.scss', './assets/scss/*.scss'], ['sass']);
 });
 
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 8080;
 
 // Set express app settings
 app.set('view engine', 'jade');
-app.set ('views', __dirname + '/views');
+app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
@@ -15,5 +15,5 @@ app.get('/', function(req, res) {
 });
 
 app.listen(port, function() {
-    console.log("NodeJS Server running on port " + port);
+  console.log("NodeJS Server running on port " + port);
 });
