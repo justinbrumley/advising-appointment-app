9ca7ffa217efe0588067b921721cc743041466c7
dragonswake <daneluquette@gmail.com>
2016-03-01 03:32:54 +0000

    Changed login and register logic to knockout

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,48 +1,50 @@
 'use strict';
-
-var $cwid = $('#cwid');
-var $password = $('#password');
-var $error = $('#login-error');
-
-$('#login-form-submit').on('click', function(e) {
-  $.blockUI({ css: { 
-    border: 'none', 
-    padding: '15px', 
-    backgroundColor: '#000', 
-    '-webkit-border-radius': '10px', 
-    '-moz-border-radius': '10px', 
-    opacity: .5, 
-    color: '#fff' 
-  },
-    message: "Logging in..."
-  });
-  $.ajax({
-    url: '/users/login',
-    accepts: 'json',
-    data: {
-      cwid: $cwid.val(),
-      password: $password.val()
-    },
-    dataType: 'json',
-    type: 'POST'
-  }).done(function(data) {
-    $.unblockUI();
-    if(data) {
-      if(data.success == true) {
-        // Success - User logged in
-        window.location.href = '/';
-      } else {
-        // Failed - Error message
-        console.log(data.message);
-        $error.text(data.message);
-        $error.show();
-        setTimeout(function() { $error.hide(); }, 4000);
-      }
-    } else {
-      // No data received. Assume error
-      console.log("no response");
-    }
-  });
+var viewModel = {
+  cwid: ko.observable(),
+  password: ko.observable(),
+  error: ko.observable(),
   
-  return false;
-});
+  login: function(e) {
+    $.blockUI({ css: { 
+      border: 'none', 
+      padding: '15px', 
+      backgroundColor: '#000', 
+      '-webkit-border-radius': '10px', 
+      '-moz-border-radius': '10px', 
+      opacity: .5, 
+      color: '#fff' 
+    },
+      message: "Logging in..."
+    });
+    $.ajax({
+      url: '/users/login',
+      accepts: 'json',
+      data: {
+        cwid: this.cwid,
+        password: this.password
+      },
+      dataType: 'json',
+      type: 'POST'
+    }).done(function(data) {
+      $.unblockUI();
+      if(data) {
+        if(data.success == true) {
+          // Success - User logged in
+          window.location.href = '/';
+        } else {
+          // Failed - Error message
+          console.log(data.message);
+          this.error(data.message);
+          setTimeout(function() { this.error(null); }.bind(this), 4000);
+        }
+      } else {
+        // No data received. Assume error
+        console.log("no response");
+      }
+    }.bind(this));
+    
+    return false;
+  }
+}
+
+ko.applyBindings(viewModel);
