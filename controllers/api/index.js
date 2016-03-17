bb0b73079d880a8fded7a07317652d5cf07f0634
Justin Brumley <justinbrumley012@gmail.com>
2016-03-17 18:11:12 -0500

    Added api endpoint to pull role list per user

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var requireAuth = require('../middleware').requireAuth;
+var _ = require('underscore');
+var async = require('async');
+
+var models = require('../../models');
+
+var UserRole = models.UserRole;
+var Role = models.Role;
 
 router.get('/', function(req, res) {
   return res.json({
@@ -8,13 +16,49 @@ router.get('/', function(req, res) {
 });
 
 /**
-* Pull privileges for the user and send back nav items
+* Pull list of roles for the logged in user.
 */
-router.get('/users/:cwid/sidebar', function(req, res) {
-  var cwid = req.params.cwid;
+router.get('/me/roles', requireAuth, function(req, res) {
+  var cwid = req.session.cwid;
 
-  return res.json({
-    options: [ 'calendar', 'settings' ]
+  async.waterfall([
+    function(callback) {
+      UserRole.findAll({
+        where: {
+          cwid: cwid
+        }
+      }).done(function(userRoles) {
+        callback(null, userRoles);
+      });
+    },
+    function(userRoles, callback) {
+      Role.findAll({
+        where: {
+          id: {
+            $in: _.pluck(userRoles, 'role_id')
+          }
+        }
+      }).done(function(roles) {
+        callback(null, roles);
+      });
+    }
+  ], function(err, roles) {
+    if(err) {
+      res.json({
+        success: false,
+        message: err
+      });
+    } else if(roles) {
+      res.json({
+        success: true,
+        roles: roles
+      });
+    } else {
+      res.json({
+        success: false,
+        message: 'No roles found'
+      });
+    }
   });
 });
 
