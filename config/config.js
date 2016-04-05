f0e09bd7ce5ca6ed20b7ec87ab695023f3127b4b
Justin Brumley <justinbrumley012@gmail.com>
2016-04-04 19:11:48 -0500

    Broke stuff

--- config/config.js
+++ config/config.js
@@ -2,25 +2,27 @@ var config = {};
 
 // Development
 config.development = {};
-config.username = 'test';
-config.password = null;
-config.database = 'test_db';
-config.host = 'localhost';
-config.dialect = 'mysql';
-config.dialectOptions = {};
-config.seederStorage = 'sequelize';
+config.development.username = 'test';
+config.development.password = null;
+config.development.database = 'test_db';
+config.development.host = 'localhost';
+config.development.dialect = 'mysql';
+config.development.dialectOptions = {};
+config.development.seederStorage = 'sequelize';
 
 // Test
-config.development = {};
-config.username = 'root';
-config.password = null;
-config.database = 'database_test';
-config.host = 'localhost';
-config.dialect = 'mysql';
+config.test.development = {};
+config.test.username = 'root';
+config.test.password = null;
+config.test.database = 'database_test';
+config.test.host = 'localhost';
+config.test.dialect = 'mysql';
 
 // Production
 config.production = {};
 config.production.url = process.env.CLEARDB_DATABASE_URL;
 config.production.dialect = 'mysql';
+config.production.seederStorage = 'sequelize';
+config.production.port = 3306;
 
 module.exports = config;
